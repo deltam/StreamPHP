ff7ab7f2031e82d951da46c8a9db822e35e37bb2
MISUMI Masaru(deltam) <deltam@gmail.com>
2015-04-04 17:28:31 +0900

    testing code checker

--- tests/StreamTest.php
+++ tests/StreamTest.php
@@ -6,7 +6,7 @@ class StreamTest extends \PHPUnit_Framework_TestCase
 {
   public function testDelayForce()
   {
-      $delay=Stream::delay(function(){return 3;});
+      $delay=Stream::delay(function() {return 3;});
 
       $this->assertInstanceOf('Closure',$delay);
       $this->assertEquals(3,Stream::force($delay));
