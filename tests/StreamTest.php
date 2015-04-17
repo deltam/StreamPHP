ab7ad1ba8f8e10da281d1a823168876798ff7bc6
MISUMI Masaru(deltam) <deltam@gmail.com>
2015-04-17 18:30:13 +0900

    modified coding style

--- tests/StreamTest.php
+++ tests/StreamTest.php
@@ -1,14 +1,21 @@
 <?php
+/**
+ * StreamTest
+ */
 
 namespace StreamPHP;
 
 class StreamTest extends \PHPUnit_Framework_TestCase
 {
-  public function testDelayForce()
-  {
-      $delay=Stream::delay(function() {return 3;});
+    public function testDelayForce()
+    {
+        $delay=Stream::delay(
+            function() {
+                return 3;
+            }
+        );
 
-      $this->assertInstanceOf('Closure',$delay);
-      $this->assertEquals(3,Stream::force($delay));
-  }
+        $this->assertInstanceOf('Closure', $delay);
+        $this->assertEquals(3, Stream::force($delay));
+    }
 }
